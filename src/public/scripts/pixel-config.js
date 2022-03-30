071990253f0108126bd9d19d3f6c81f2aa607c19
Masquentin <1292382664@qq.com>
2022-03-30 11:36:45 +0800

    Update pixel-config.js

--- src/public/scripts/pixel-config.js
+++ src/public/scripts/pixel-config.js
@@ -9,11 +9,11 @@
     // Pixel Settings
     var Pixel = {
         PIXEL_SERVER: location.origin.replace(/^http/, 'ws'),
-        CANVAS_WIDTH: 50, // The width and height must be the same as the values set for the server
-        CANVAS_HEIGHT: 50,
+        CANVAS_WIDTH: 512, // The width and height must be the same as the values set for the server
+        CANVAS_HEIGHT: 512,
         CANVAS_INITIAL_ZOOM: 20,
         CANVAS_MIN_ZOOM: 10,
-        CANVAS_MAX_ZOOM: 40,
+        CANVAS_MAX_ZOOM: 80,
         CANVAS_COLORS: ["#eeeeee", "red", "orange", "yellow", "green", "blue", "purple", "#614126", "white", "black"],
         CANVAS_ELEMENT_ID: "pixelCanvas",
 
